0a9c8affa0d82d07d75fa8b4311e5b6cdb38148a
myselfshravan <shravanrevanna@gmail.com>
2025-04-07 23:48:40 +0530

    fix: add eslint directive to suppress warning for useMockData export

--- src/components/MockDataProvider.tsx
+++ src/components/MockDataProvider.tsx
@@ -176,6 +176,7 @@ export const MockDataProvider: React.FC<MockDataProviderProps> = ({
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useMockData = () => {
   const context = useContext(MockDataContext);
   if (context === undefined) {
